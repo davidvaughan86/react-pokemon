9a13c2fe6ff70595aeba076b182117f4661e2423
davidvaughan86 <david.vaughan86@gmail.com>
2021-01-22 15:36:24 -0500

    updates fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Pokebutton from './pokebutton'
 import Header from './header'
+import Pokemon from './pokemon'
 
 
 
@@ -9,23 +10,16 @@ import Header from './header'
 
 
 function App() {
-  const randomNumber = Math.floor(Math.random() * 898);
-  async function getPoke(randomNumber) {
-    const pokePromise = fetch('https://pokeapi.co/api/v2/pokemon/${randomNumber}', {
+  async function getPoke() {
+    const randomNumber = Math.floor(Math.random() * 898);
+    const pokePromise = fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`, {
       headers: { 
         Accept: 'application/json'
-      
-        
       }
-   
-  
     });
-    
     const response = await pokePromise;
     const pokeData = await response.json();
-  
     console.log(pokeData.name)
-
   }
     
   
@@ -35,6 +29,7 @@ function App() {
       <header className="App-header">
       <Header />
       <Pokebutton poke={getPoke} />
+      <Pokemon />
       </header>
     </div>
   );
---
+++ src/pokemon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App'
+
+export default function pokemon (props) {
+    return(
+        <div className="container">
+            <p>here is your pokemon {props.name}</p>
+        </div>
+    )
+
+
+
+}
+
